9720437d82a008a268d730f0239b028f935e7b71
Peter Rogers <peter.rogers@mtuity.com>
2014-09-24 23:31:16 -0500

    Ripping out the sample code.

--- johari.js
+++ johari.js
@@ -1,18 +1,6 @@
 adjectiveArray = [{name: "happy"},{name:"sad"},{name:"angry"},{name:"afraid"}];
 
 if (Meteor.isClient) {
-    Template.hello.greeting = function() {
-        return "Welcome to johari.";
-    };
-
-    Template.hello.events({
-        'click input': function() {
-            // template data, if any, is available in 'this'
-            if (typeof console !== 'undefined')
-                console.log("You pressed the button");
-        }
-    });
-
     Template.adjectives.adjective = function() {
         return adjectiveArray;
     };
